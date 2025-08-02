831ed4189f680c1cf50d3e960a1b70da5dc1a6ee
Manuel Schneider <manuelschneid3r@gmail.com>
2025-08-03 01:33:08 +0200

    Add maintainer field

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ md_description = "Manage docker images and containers"
 md_license = "MIT"
 md_url = "https://github.com/albertlauncher/albert-plugin-python-docker"
 md_authors = "@manuelschneid3r"
+md_maintainers = ["@manuelschneid3r"]
 md_bin_dependencies = "docker"
 md_lib_dependencies = "docker"
 
